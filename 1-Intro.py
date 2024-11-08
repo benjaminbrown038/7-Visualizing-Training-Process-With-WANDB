1fb228e660e330d0b028478b7859e456b9b89eb7
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-11-08 16:15:03 -0500

    Create 1-Intro.py

---
+++ 1-Intro.py
@@ -0,0 +1,18 @@
+!pip install wandb
+
+import wandb
+import random
+import math
+
+wandb.login()
+wandb.init()
+wandb.log()
+wandb.finish()
+wandb.Table()
+wandb.summary()
+wandb.finish()
+wandb.log_model()
+
+
+
+
